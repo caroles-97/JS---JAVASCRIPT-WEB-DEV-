b750633cda23001c6938d9c3a2b67c29d46552fb
Carol <carol.tiago9793@gmail.com>
2026-04-01 20:59:43 -0300

    JS-aula1

---
+++ script.js
@@ -0,0 +1,25 @@
+// alert("Hello World");  
+// // prompt("Qual é o seu nome?");
+// // confirm("Você deseja sair?");
+// console.log("Aviso!")
+// // alert  - tipo um alerta , pop-up 
+// // console.log - para aparecer no console
+
+// let nome = "Carolina Kiyomi";
+// const pi = 3.1415;
+// alert(nome);
+// // número não precisa de "" , a não ser que seja um texto
+
+// console.log(1/0);
+// // variável Infinity
+
+// console.log(1/"a");
+// // variável NaN == indica que não é uma operação válida
+
+// console.log(typeof pi) 
+// // informa o tipo de variável no console
+
+let nome2 = prompt("Qual é o seu nome?")
+alert(nome2)
+let idade = prompt ("Qual é a sua idade?")
+alert(`Seu nome é ${nome2} e sua idade é ${idade}`)
