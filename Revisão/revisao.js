9f038ca0bfa7281d0f86b5d4a2b9b96728a248df
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-08 19:55:07 -0300

    doc:revisao

---
+++ Revisão/revisao.js
@@ -0,0 +1,7 @@
+// fazer um programa que pergunta o nome2, idade e curso e dps apresenta um alert com a frase: "Olá, meu nome é ____, tenho __ anos e estudo ___ na FIAP"
+
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual sua idade?")
+let curso = prompt ("Qual curso está estudando?")
+
+alert (`Olá, meu nome é  ${nome}, tenho  ${idade} e estudo ${curso} na FIAP`)
